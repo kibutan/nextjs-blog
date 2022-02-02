11a3055b8eb8659be9c06d5130ad2c7815f1d869
kibutan <s1170230@gmail.com>
2022-02-02 15:18:58 +0900

    tutorial

---
+++ pages/_app.js
@@ -0,0 +1,8 @@
+
+import "../styles/global.css"
+function MyApp({ Component, pageProps }){
+    return <Component {...pageProps} />
+}
+export default MyApp
+
+// https://nextjs.org/learn/basics/assets-metadata-css/global-styles
